88aa6cafabf5ead251c7ee4510e42fe421129335
microdotmatrix <liquid.knives.2@gmail.com>
2024-10-10 14:33:30 -0500

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,20 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://greenlionpartners.com",
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    react(),
+  ],
+  // output: "hybrid",
+
+  // adapter: node({
+  //   mode: "standalone",
+  // }),
+});
---
+++ src/components/icons/index.ts
@@ -0,0 +1,5 @@
+export { default as Copyright } from "./copyright.astro";
+export { default as Facebook } from "./facebook.astro";
+export { default as Instagram } from "./instagram.astro";
+export { default as LinkedIn } from "./linkedin.astro";
+export { default as X } from "./x.astro";
